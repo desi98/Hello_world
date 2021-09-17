bfd562273d58874a025a33f013a4b8e6caa67a7e
desi98 <90495152+desi98@users.noreply.github.com>
2021-09-18 00:28:52 +0530

    Add files via upload

---
+++ university entry.py
@@ -0,0 +1,29 @@
+print("                          ******UNIVERSITY OF HOGWARTS******            ")
+print("             #######################################################################")
+
+
+
+
+
+fname=input("Enter your first name : ")
+sname=input("Enter your second name: ")
+print(  "    Hi!..",sname           )
+print("              please fill out the form below to find out if you have been selected for the UNIVERSITY OF HOGWARTS....")
+
+
+
+
+age=int(input("Enter your age :"))
+Id=int(input("Enter your exam ID :"))
+score=float(input("Enter your Z-SCORE :"))
+blood= input("Enter your blood group: ")
+if score>0.6000:
+    print("you are selected for the university of HOGWARTS...." ," The HOGWARTS EXPRESS is reserved for you on september 1st. please report to Platform 9 3/4 before nine o'clock ","---The magical hat chose house u own----",sep="\n")
+
+    
+else:
+    print("NOT SELECTED... PLEASE TRY AGAIN NEXT SHIE")
+
+
+
+print("press enter to exit")
